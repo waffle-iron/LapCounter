1bada2afbdb7f65e289360f5e6500c46aef3918f
honeyworks <honeyworks@users.noreply.github.com>
2017-05-19 14:53:15 +0100

    Update SerialRead.py

--- SerialRead.py
+++ SerialRead.py
@@ -1,6 +1,19 @@
 import serial
+import os
 
-ser = serial.Serial('/dev/ttyAMA0',9600)
+os.system("ls /dev | grep ttyA")
+
+print "Potential inputs:"
+print "1. ACM0"
+print "2. AMA0"
+print ""
+
+option = str(raw_input("Please choose input: "))
+
+if option==str(1):
+	ser = serial.Serial('/dev/ttyACM0',9600)
+elif option==str(2):
+	ser = serial.Serial('/dev/ttyAMA0',9600)
 
 while True:
 	x=ser.readline()
